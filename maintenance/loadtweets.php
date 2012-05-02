b0c3d4e15b2c28c813e6c1a7677f2956248bceaa
Matthias Vill <github@cconstruct.de>
2012-05-03 01:20:52 +0200

    Updated favorite-fetching

--- maintenance/loadtweets.php
+++ maintenance/loadtweets.php
@@ -149,6 +149,7 @@
 				if(!$q){
 					dieout(l(bad("DATABASE ERROR: " . $db->error())));
 				}
+				// Add to Word-Index
 				$text = $tweet['text'];
 				$te   = $tweet['extra'];
 				if(is_string($te)){ $te = @unserialize($tweet['extra']); }
@@ -186,33 +187,51 @@
 		}
 		
 		$tweets = Array();
+		$maxID = false;
 		
 		// Retrieve favorites
-		for($i = 0; $i < $pages; $i++){
-			$path = "1/favorites.json?" . $p . "&count=" . $maxCount . ($i > 0 ? "&page=" . $i : "");
-			$path = "1/favorites.json?" . $p . "&count=" . $maxCount . ($sinceID ? "&since_id=" . $sinceID : "") . ($maxID ? "&max_id=" . $maxID : "");
+		do {
+			// Determine path to Twitter favorites resource
+			$path =	"1/favorites.json?" . $p . // <-- user argument
+					"&include_rts=true&include_entities=true&count=" . $maxCount .
+					($sinceID ? "&since_id=" . $sinceID : "") . ($maxID ? "&max_id=" . $maxID : "");
+			// Announce
 			echo l("Retrieving page <strong>#" . ($i+1) . "</strong>: <span class=\"address\">" . ls($path) . "</span>\n");
+			// Get data
 			$data = $twitterApi->query($path);
+			// Drop out on connection error
 			if(is_array($data) && $data[0] === false){ dieout(l(bad("Error: " . $data[1] . "/" . $data[2]))); }
+			
+			// Start parsing
 			echo l("<strong>" . ($data ? count($data) : 0) . "</strong> new favorites on this page\n");
-			if(!$data){ break; } // No more tweets
-			echo l("<ul>");
-			foreach($data as $tweet){
-				echo l("<li>" . $tweet->id_str . " " . $tweet->created_at . "</li>\n");
-				$tweets[] = $twitterApi->transformTweet($tweet);
-				$maxID    = sprintf("%.0F", (float)((float)$tweet->id - 1));
+			if(!empty($data)){
+				echo l("<ul>");
+				foreach($data as $i => $tweet){
+					// Shield against duplicate tweet from max_id
+					if(!IS64BIT && $i == 0 && $maxID == $tweet->id_str){ unset($data[0]); continue; }
+					// List tweet
+				    echo l("<li>" . $tweet->id_str . " " . $tweet->created_at . "</li>\n");
+					// Create tweet element and add to list
+				    $tweets[] = $twitterApi->transformTweet($tweet);
+					// Determine new max_id
+					$maxID    = $tweet->id_str;
+					// Subtracting 1 from max_id to prevent duplicate, but only if we support 64-bit integer handling
+					if(IS64BIT){
+						$maxID = (int)$tweet->id - 1;
+					}
+				}
+				echo l("</ul>");
 			}
-			echo l("</ul>");
-			if(count($data) < ($maxCount - 50)){
+			/*if(count($data) < ($maxCount - 50)){
 				echo l("We've reached last page\n");
 				break;
-			}
-		}
+			}*/
+		} while(!empty($data));
 		
 		if(count($tweets) > 0){
 			// Ascending sort, oldest first
 			$tweets = array_reverse($tweets);
-			echo l("<strong>All tweets collected. Reconnecting to DB...</strong>\n");
+			echo l("<strong>All favorites collected. Reconnecting to DB...</strong>\n");
 			$db->reconnect(); // Sometimes, DB connection times out during tweet loading. This is our counter-action
 			echo l("Inserting into DB...\n");
 			$error = false;
@@ -221,19 +240,6 @@
 				if(!$q){
 					dieout(l(bad("DATABASE ERROR: " . $db->error())));
 				}
-				/*
-				// For now I skip word indexing
-				$text = $tweet['text'];
-				$te   = $tweet['extra'];
-				if(is_string($te)){ $te = @unserialize($tweet['extra']); }
-				if(is_array($te)){
-					// Because retweets might get cut off otherwise
-					$text = (array_key_exists("rt", $te) && !empty($te['rt']) && !empty($te['rt']['screenname']) && !empty($te['rt']['text'])) 
-						? "RT @" . $te['rt']['screenname'] . ": " . $te['rt']['text'] 
-						: $tweet['text'];
-				}
-				$search->index($db->insertID(), $text);
-				*/
 			}
 			echo !$error ? l(good("Done!\n")) : "";
 		} else {
