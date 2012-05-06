9b92bb68460656ab20ef9337197e506b0155f0cd
Matthias Vill <github@cconstruct.de>
2012-05-06 19:35:23 +0200

    More information on who faved

--- fav_index.php
+++ fav_index.php
@@ -8,7 +8,7 @@
 	
 	$filterMode = "favorites";
 	
-	$q = $db->query("SELECT `".DTP."favorites`.*, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."favorites` LEFT JOIN `".DTP."tweetusers` ON `".DTP."favorites`.`favinguserid` = `".DTP."tweetusers`.`userid` ORDER BY `".DTP."favorites`.`time` DESC LIMIT 25");
+	$q = $db->query("SELECT `".DTP."favorites`.*, `".DTP."tweetusers`.`screenname` AS faving_screenname, `".DTP."tweetusers`.`realname` AS faving_screenname, `".DTP."tweetusers`.`profileimage` AS faving_profileimage FROM `".DTP."favorites` LEFT JOIN `".DTP."tweetusers` ON `".DTP."favorites`.`favinguserid` = `".DTP."tweetusers`.`userid` ORDER BY `".DTP."favorites`.`time` DESC LIMIT 25");
 	$pageHeader = "Recent favorites";
 	$home       = true;
 	require "inc/header.php";
